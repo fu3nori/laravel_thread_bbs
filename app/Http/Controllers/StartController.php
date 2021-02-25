0cd96a745ba146bb8a9f26018e4ebdedfca12178
fu3nori <cap.fuminori.usui@gmail.com>
2021-02-25 17:37:28 +0900

    articleのマイグレーションファイルを作成した。 ユーザー認証を実装した

--- app/Http/Controllers/StartController.php
+++ app/Http/Controllers/StartController.php
@@ -4,14 +4,21 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Article;
+use Illuminate\Support\Facades\Auth;
+
 
 class StartController extends Controller
 {
     //index
     public function index(){
-        $articles = Article::first();
+        $articles = Article::first()->toArray();
         var_dump($articles);
-
+        echo "<br>---<br>";
+        if (Auth::check()) {
+            echo "ログインしてます";
+        }
         return view('start');
     }
+
+
 }
