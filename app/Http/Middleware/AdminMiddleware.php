9e5ddeabc454a862679ed75b98a0857691c771b2
fu3nori <cap.fuminori.usui@gmail.com>
2021-05-21 22:34:16 +0900

    ロールによってアクセス可能とアクセス不可をadminに仕掛けた

--- app/Http/Middleware/AdminMiddleware.php
+++ app/Http/Middleware/AdminMiddleware.php
@@ -20,16 +20,13 @@ class AdminMiddleware
         $user_id = Auth::id();
         $role = DB::table('users')->where('id', $user_id)->value('role');
         var_dump($role);
-        /**
-        foreach($user->roles as $role){
-            // ロール値が100(Admin)ならアクセス可能
-            if($role->role=='100'){
-                return $next($request);
-            }else{
-            // ロール値が100意外ならアクセス不可能
-                abort(404);
-            }
-        } */
+        if($role=='100'){
+            // ロール値が100(admin)ならアクセス可能
+            return $next($request);
+        }else{
+            // それ以外はアクセス不可
+            abort(403);
+        }
         return $next($request);
     }
 }
